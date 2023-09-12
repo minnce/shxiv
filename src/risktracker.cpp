1a877980b8c8d7a7f402168cce359059fb9b5f2f
Chris Minn <105378107+minnce@users.noreply.github.com>
2023-09-11 19:52:54 -0500

    Update risktracker.cpp

--- src/risktracker.cpp
+++ src/risktracker.cpp
@@ -7,7 +7,6 @@
 RiskTracker::RiskTracker(float x, std::vector <Trade> trades) : totalRisk(x), pendingTrades(trades) {};
 
 int RiskTracker::updateRisk() {
-    this->totalRisk = 0;
     float runningSum = 0;
     for (const auto &x: this->pendingTrades) {
         if (x.side) {
