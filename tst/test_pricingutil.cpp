78fdaef67c6bd683cc845bb4c5f20766f1bd2f44
minnce <chrisminn205@gmail.com>
2023-09-14 13:19:00 -0500

    Wtf

--- tst/test_pricingutil.cpp
+++ tst/test_pricingutil.cpp
@@ -1,5 +1,13 @@
 #include <gtest/gtest.h>
+#include "pricingutil.h"
 
 TEST(sampleTest, sample) {
     EXPECT_EQ(4, 4);
 }
+
+TEST(initTest, yay) {
+    PricingUtil pricingUtil;
+    float res = pricingUtil.calcVal(1.0, 0.1, 2.0);
+    EXPECT_NEAR(res, 2.0, 1e-4);
+}
+
